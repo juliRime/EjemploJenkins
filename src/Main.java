040f39e99f99558ff8b0fd4dc4686f6be868dde9
Juliana Rios Medina <julianarios0796gmail.com>
2023-10-24 20:40:46 -0500

    ajuste

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("mi novio es un mocoso");
+
+        Ajjjj ajam = new Ajjjj();
+        ajam.mensaje();
     }
 }
